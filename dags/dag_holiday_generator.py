8718c6db47c991bd691fd701c23d4dd867391bf2
levyvix <levy.vix@gmail.com>
2024-01-28 13:45:21 -0300

    to amazon

--- dags/dag_holiday_generator.py
+++ dags/dag_holiday_generator.py
@@ -1,13 +1,21 @@
 import datetime
-import logging
-import requests
 import json
+import logging
+
+import requests
 from airflow.decorators import dag, task
 from airflow.operators.bash import BashOperator
-from airflow.operators.python import get_current_context, BranchPythonOperator
-from airflow.operators.empty import EmptyOperator
 from airflow.operators.email import EmailOperator
-from pandas import json_normalize, DataFrame, concat
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import BranchPythonOperator, get_current_context
+from airflow.providers.amazon.aws.operators.s3 import S3CreateBucketOperator
+from airflow.providers.amazon.aws.transfers.local_to_s3 import (
+    LocalFilesystemToS3Operator,
+)
+from pandas import json_normalize
+
+S3_CONN_ID = "aws_default"
+BUCKET_NAME = "airflow-levy"
 
 
 @dag(
@@ -20,13 +28,9 @@ from pandas import json_normalize, DataFrame, concat
 def dag_holiday_generataor(
     url_base: str, endpoint: str, api_key: str, country: str, year: str
 ):
-    @task(retries = 3)
+    @task(retries=3)
     def get_json_response() -> bool:
         context = get_current_context()
-
-        date = context.get("ds_nodash")
-
-        logging.info(date)
 
         # parse all arguments into string
         url_base_str = url_base.resolve(context)
@@ -59,31 +63,30 @@ def dag_holiday_generataor(
                 for holiday in holidays:
                     yield holiday
 
-
         batch_size = 1000
-        header_written=False
+        header_written = False
+
         # Convert the generator to a DataFrame
         for i, holiday in enumerate(get_holidays()):
             dataframe = json_normalize(holiday)
 
-            if i % batch_size ==  0:
-                mode = 'w' if not header_written else 'a'
+            if i % batch_size == 0:
+                mode = "w" if not header_written else "a"
                 dataframe.to_csv(
-                    f"/tmp/holidays_{year_str}.csv", index=False, mode=mode, header=not header_written
+                    f"/tmp/holidays_{year_str}.csv",
+                    index=False,
+                    mode=mode,
+                    header=not header_written,
                 )
             else:
                 dataframe.to_csv(
-                    f"/tmp/holidays_{year_str}.csv", index=False, mode="a", header=not header_written
+                    f"/tmp/holidays_{year_str}.csv",
+                    index=False,
+                    mode="a",
+                    header=not header_written,
                 )
 
         return "done"
-
-    bash_operator = BashOperator(
-        task_id="date",
-        bash_command="echo  {{ ds }}",
-    )
-
-    json_response = get_json_response()
 
     email_operator = EmailOperator(
         task_id="send_email",
@@ -95,23 +98,46 @@ def dag_holiday_generataor(
     def _check_status_code(response_code):
         return "write_dataframe" if response_code else "send_email"
 
+    json_response = get_json_response()
+
     branch_operator = BranchPythonOperator(
         task_id="is_status_code_200",
         python_callable=_check_status_code,
         op_args=[json_response],
     )
 
+    create_bucket = S3CreateBucketOperator(
+        task_id="create_bucket",
+        bucket_name=BUCKET_NAME,
+        region_name="us-east-1",
+        aws_conn_id=S3_CONN_ID,
+    )
+
+    to_s3 = LocalFilesystemToS3Operator(
+        task_id="to_s3",
+        filename="/tmp/holidays_{{params.year}}.csv",
+        dest_key=f"s3://{BUCKET_NAME}"
+        + "/{{ params.year }}/holidays_{{params.year}}.csv",
+        replace=True,
+        aws_conn_id=S3_CONN_ID,
+    )
+
+    remove_local_files = BashOperator(
+        task_id="remove_local_files",
+        # delete files from /tmp
+        bash_command="rm -rf /tmp/holidays_{{params.year}}.csv /tmp/holidays.json",
+    )
+
     empty = EmptyOperator(task_id="empty")
 
-    done = write_dataframe()
+    write_dataframe_done = write_dataframe()
 
     # write_dataframe()
     # done = write_dataframe(holiday_generator)
-    json_response >> branch_operator >> [done, email_operator]
+    json_response >> branch_operator >> [write_dataframe_done, email_operator]
 
+    write_dataframe_done >> create_bucket >> to_s3 >> remove_local_files
     email_operator >> empty
-    done >> bash_operator
-    
 
 
 dag_holiday_generataor(
